8cd87b7adbce35ce330a62a4ccdbe72b89601287
shlocko <mcdude22@gmail.com>
2022-06-14 16:47:01 -0700

    Added minor documentation

--- src/commands.rs
+++ src/commands.rs
@@ -1,9 +1,12 @@
 use crate::prelude::*;
 
+// Takes in the string from user input, 
+// splits it into a command (Option) and args (Vector of Strings)
 pub fn parse_command(input: String) -> (Option<String>, Vec<String>) {
     let split = input.split(' ');
     let mut opt: Option<String> = None;
     let mut args: Vec<String> = Vec::new();
+    
     for s in split {
         if opt == None {
             opt = Some(s.to_lowercase());
@@ -14,6 +17,8 @@ pub fn parse_command(input: String) -> (Option<String>, Vec<String>) {
     (opt, args)
 }
 
+// Send user input to parse_command(), then matches the command Option
+// to execute the associated function
 pub fn run_command(input: String) {
     let (cmd, args) = parse_command(input);
     match cmd.as_deref() {
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use prelude::*;
 fn main() {
     println!("Enter a command: (Type 'help' for help)");
 
+    // Read a command from the user
     let mut cmd = String::new();
     stdin()
         .read_line(&mut cmd)
