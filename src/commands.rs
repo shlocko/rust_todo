f02a98f5f01f7307b1520fca689cc1533593328e
shlocko <mcdude22@gmail.com>
2022-06-16 11:30:08 -0700

    Reworked command parsing and executing

--- src/commands.rs
+++ src/commands.rs
@@ -1,32 +1,56 @@
 use crate::prelude::*;
+use std::collections::HashMap;
 
 // Takes in the string from user input, 
 // splits it into a command (Option) and args (Vector of Strings)
-pub fn parse_command(input: String) -> (Option<String>, Vec<String>) {
+
+pub fn parse_input(input: String) -> Option<(String, Vec<String>)> {
     let split = input.split(' ');
-    let mut opt: Option<String> = None;
+    let mut cmd: String = String::new();
     let mut args: Vec<String> = Vec::new();
+    let mut opt: Option<(String, Vec<String>)> = None;
     
-    for s in split {
-        if opt == None {
-            opt = Some(s.to_lowercase());
-        } else {
-            args.push(s.to_string());
+    if !input.is_empty() {
+        for s in split {
+            if cmd.is_empty() {
+                cmd = s.to_lowercase();
+            } else {
+                args.push(s.to_string());
+            }
         }
+        opt = Some((cmd, args));
     }
-    (opt, args)
+    opt
 }
 
 // Send user input to parse_command(), then matches the command Option
 // to execute the associated function
 pub fn run_command(input: String) {
-    let (cmd, args) = parse_command(input);
-    match cmd.as_deref() {
-        Some("test") => { // Test command, outputs args for testing
+
+    // Create a HashMap of commands
+    let mut commands: HashMap<String, fn(Vec<String>)> = HashMap::new();
+
+    // Fill that HashMap of commands
+    commands.insert(
+        "test".to_string(),
+        |args: Vec<String>| {
             for s in args {
-                println!("{}",s);
+                println!("{}", s);
+            }
+        }
+        );
+     
+    let parsed_command = parse_input(input); // Prarse the input
+
+    match parsed_command {
+        Some((cmd, args)) => { // Checks that there was input
+            let func = commands.get(&cmd); // Pulls the command funciton out of the HashMap
+            match func {
+                Some(func) => func(args), // If found, execute the command
+                None => println!("{} is not a valid command", cmd)
             }
         },
-        _ => println!("Not a command")
+        None => println!("You did not input a command")
     }
+
 }
